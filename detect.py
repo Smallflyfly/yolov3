94d441ef7efadac153b6359d5adfaaf307c57b5b
Fangpf <fangpf5@chinaunicom.cn>
2019-11-18 11:33:04 +0800

    增加视频读取

--- detect.py
+++ detect.py
@@ -9,7 +9,7 @@ from utils.utils import *
 def detect(save_txt=False, save_img=False):
     img_size = (320, 192) if ONNX_EXPORT else opt.img_size  # (320, 192) or (416, 256) or (608, 352) for (height, width)
     out, source, weights, half, view_img = opt.output, opt.source, opt.weights, opt.half, opt.view_img
-    webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
+    webcam = source == '0' or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt') or source.endswith(".mp4")
 
     # Initialize
     device = torch_utils.select_device(device='cpu' if ONNX_EXPORT else opt.device)
